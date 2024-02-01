9be1840d508df6149cb3bc2b6222d659124ac6a5
Jose Lara <jose@joselara.com>
2024-02-01 08:26:29 -0800

    Resolve Header and Layout Alignment Problems

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,15 @@
 <div class="pointer-events-none flex flex-col">
+    <x-container>
+        <div class="flex flex-1 justify-end md:justify-center">
+            <!-- Desktop Nav -->
+        </div>
+    </x-container>
+</div>
+
+<x-container>
     <div class="mt-24 mb-4">
         <x-avatar />
     </div>
-</div>
+</x-container>
 
 {{ $slot }}
