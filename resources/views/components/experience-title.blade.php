e07122a4a4c46a92caf02f148e480151ee1c5092
Jose Lara <jose@joselara.com>
2024-02-01 09:32:05 -0800

    Create experience-title blade compontent

---
+++ resources/views/components/experience-title.blade.php
@@ -0,0 +1,15 @@
+<div class="sm:flex justify-between items-end">
+    <div class="text-3xl font-bold tracking-tight text-zinc-800 sm:text-4xl block">
+        Experience
+    </div>
+    <div class="inline-block">
+        <a class="inline-flex items-center shadow-sm gap-2 justify-center rounded-md py-2 px-5 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 group mt-6 w-full border"
+            href="{{ assetUrl('Jose-Lara_CV.pdf') }}" target="_blank">
+            Download CV <svg viewBox="0 0 16 16" fill="none" aria-hidden="true"
+                class="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600">
+                <path d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round" />
+            </svg>
+        </a>
+    </div>
+</div>
