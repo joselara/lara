614dc81a01ab2ba83a84b8de6f7ba1cb5572d7ff
Jose Lara <jose@joselara.com>
2024-02-01 09:27:13 -0800

    Implement assetUrl for static assets

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,3 +1,3 @@
 <div class="h-16 w-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
-    <img src="/me.jpg" class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800">
+    <img src="{{ assetUrl('me.jpg') }}" class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800">
 </div>
