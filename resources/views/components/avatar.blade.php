70be5ea1d857e1a09d0c795f305098358e672282
Jose Lara <jose@joselara.com>
2024-02-01 16:20:14 -0800

    Update shadow to drop-shadow class

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,3 +1,4 @@
-<div class="h-16 w-16 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
+<div
+    class="h-16 w-16 rounded-full bg-white/90 p-0.5 drop-shadow-lg drop-shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
     <img src="{{ assetUrl('me.jpg') }}" class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800">
 </div>
--- resources/views/components/experience-title.blade.php
+++ resources/views/components/experience-title.blade.php
@@ -3,7 +3,7 @@
         Experience
     </div>
     <div class="inline-block">
-        <a class="inline-flex items-center shadow-sm gap-2 justify-center rounded-md py-2 px-5 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 group mt-6 w-full border"
+        <a class="inline-flex items-center drop-shadow-sm gap-2 justify-center rounded-md py-2 px-5 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 group mt-6 w-full border"
             href="{{ assetUrl('Jose-Lara_CV.pdf') }}" target="_blank">
             Download CV <svg viewBox="0 0 16 16" fill="none" aria-hidden="true"
                 class="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600">
