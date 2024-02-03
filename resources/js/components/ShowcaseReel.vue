ec7f5fc784adbd8f0d4a73eddea704e9a81ebf26
Jose Lara <jose@joselara.com>
2024-02-02 20:04:18 -0800

    Add heroicons

--- resources/js/components/ShowcaseReel.vue
+++ resources/js/components/ShowcaseReel.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { defineProps, ref, onMounted } from 'vue';
+import { defineProps, ref, onMounted } from "vue";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/vue/24/solid";
 
 const props = defineProps<{
-  images: string[];
+    images: string[];
 }>();
 
 const rotations = ref<string[]>([]);
@@ -10,67 +11,68 @@ const currentIndex = ref(0);
 const duplicatedImages = ref<string[]>([]);
 
 function generateRotations() {
-  if (duplicatedImages.value.length) {
-    rotations.value = Array.from({ length: duplicatedImages.value.length }, (_, index) =>
-      index % 2 === 0 ? `rotate(2deg)` : `rotate(-2deg)`
-    );
-  }
+    if (duplicatedImages.value.length) {
+        rotations.value = Array.from(
+            { length: duplicatedImages.value.length },
+            (_, index) => (index % 2 === 0 ? `rotate(2deg)` : `rotate(-2deg)`)
+        );
+    }
 }
 
 function nextImage() {
-  currentIndex.value = (currentIndex.value + 1) % props.images.length;
+    currentIndex.value = (currentIndex.value + 1) % props.images.length;
 }
 
 function prevImage() {
-  currentIndex.value = (currentIndex.value - 1 + props.images.length) % props.images.length;
+    currentIndex.value =
+        (currentIndex.value - 1 + props.images.length) % props.images.length;
 }
 
 onMounted(() => {
-  duplicatedImages.value = [...props.images, ...props.images];
-  generateRotations();
+    duplicatedImages.value = [...props.images, ...props.images];
+    generateRotations();
 });
-
 </script>
 
 <template>
-   <div className="mt-16 sm:mt-20">
-        <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-
-
+    <div className="mt-16 sm:mt-20">
+        <div
+            className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8"
+        >
             <div
-        v-for="(image, imageIdx) in duplicatedImages"
-        :key="imageIdx"
-        class="relative cursor-pointer aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl border drop-shadow-lg"
-        :style="{ transform: `translateX(-${100 * currentIndex}%) ${rotations[imageIdx % rotations.length]}` }"
-      >
-        <img :src="image" class="absolute inset-0 h-full w-full object-cover">
-      </div>
-
-
+                v-for="(image, imageIdx) in duplicatedImages"
+                :key="imageIdx"
+                class="relative cursor-pointer aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl border drop-shadow-lg"
+                :style="{
+                    transform: `translateX(-${100 * currentIndex}%) ${
+                        rotations[imageIdx % rotations.length]
+                    }`,
+                }"
+            >
+                <img
+                    :src="image"
+                    class="absolute inset-0 h-full w-full object-cover"
+                />
+            </div>
         </div>
 
-        <div class="sm:px-8">
+        <div class="sm:px-8 mt-10">
             <div class="mx-auto max-w-7xl lg:px-8">
-                <div class="relative px-4 sm:px-8 lg:px-12">
-                    <div class="mx-auto">
-                        <div
+                <div class="mx-auto flex items-center space-x-4 ml-auto">
+                    <div
+                        class="mx-auto flex items-center space-x-4 select-none"
+                    >
+                        <ArrowLeftIcon
                             @click="prevImage"
-                            class="absolute left-4 top-1/2 transform -translate-y-1/2 cursor-pointer text-zinc-700"
-                        >
-                            <!-- Left arrow icon -->
-                            Left
-                        </div>
-                        <div
+                            class="w-8 h-8 cursor-pointer text-zinc-700"
+                        />
+                        <ArrowRightIcon
                             @click="nextImage"
-                            class="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer text-zinc-700"
-                        >
-                            <!-- Right arrow icon -->
-                            Right
-                        </div>
+                            class="w-8 h-8 cursor-pointer text-zinc-700"
+                        />
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </template>
