127bf84f9077c788576a39b625d0a7fa927431d8
Jose Lara <jose@joselara.com>
2024-02-03 20:21:46 -0800

    Update app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
 import ShowcaseReel from "./components/ShowcaseReel.vue";
-import LightBox from "./components/LightBox.vue";
 
 createApp({}).component("ShowcaseReel", ShowcaseReel).mount("#app");
