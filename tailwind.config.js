988bedf4af74a35289b084a4e86339287146eafc
Jose Lara <jose@joselara.com>
2024-02-03 19:48:56 -0800

    Add headlessui

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,8 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/aspect-ratio"),
+    ],
 };
